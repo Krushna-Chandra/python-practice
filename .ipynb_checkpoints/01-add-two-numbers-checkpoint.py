e6a651b29e1e0edd34d3f00cd6d8fa80e6f5c6e3
Krushna Chandra Bindhani <kcbindhani90@gmail.com>
2026-01-15 16:17:16 +0530

    commit

---
+++ .ipynb_checkpoints/01-add-two-numbers-checkpoint.py
@@ -0,0 +1,4 @@
+# This program takes two integers as input from the user and prints their sum.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
